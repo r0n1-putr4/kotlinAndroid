1e05185975d4cc22429daf724473f25ee58c0344
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2024-09-23 09:38:31 +0700

    latihan

--- app/src/main/java/roni/putra/materipraktek/MainActivity.kt
+++ app/src/main/java/roni/putra/materipraktek/MainActivity.kt
@@ -1,8 +1,11 @@
 package roni.putra.materipraktek
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -12,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         //tes
         setContentView(R.layout.activity_main)
-        supportActionBar!!.title="Belajar Kotlin"
+
+        val btnProses = findViewById<Button>(R.id.btnProses)
+
+        btnProses.setOnClickListener {
+            val intent = Intent(this@MainActivity,MenuActivity::class.java)
+            intent.putExtra("","")
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/roni/putra/materipraktek/MenuActivity.kt
+++ app/src/main/java/roni/putra/materipraktek/MenuActivity.kt
@@ -3,6 +3,8 @@ package roni.putra.materipraktek
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +13,23 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+        val toolbarMenu = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbarMenu)
+
+        supportActionBar?.apply {
+            title = "Toolbar Back Button Example"
+            setDisplayHomeAsUpEnabled(true)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish();
+        return true;
     }
 }
