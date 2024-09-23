9a14eec4935ec595a1c7226047402465bb4117fa
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2024-09-23 08:49:28 +0700

    belajar

--- app/src/main/java/roni/putra/materipraktek/MainActivity.kt
+++ app/src/main/java/roni/putra/materipraktek/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         //tes
         setContentView(R.layout.activity_main)
+        supportActionBar!!.title="Belajar Kotlin"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
